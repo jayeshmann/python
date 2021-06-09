0d928f5668e545e585d0dbe88bd506ccf4d8d9f8
Jayesh Mann <jayeshmann06@gmail.com>
2021-06-09 15:33:04 +0530

    arr to list

--- l1q9.py
+++ l1q9.py
@@ -1,16 +1,16 @@
 c0 = int(input())
 count = 0
-arr = []
+list = []
 
 while(c0 != 1):
     if(c0 % 2 == 0):
         c0 = c0//2
         count += 1
-        arr.append(c0)
+        list.append(c0)
     else:
         c0 = 3*c0+1
         count += 1
-        arr.append(c0)
+        list.append(c0)
 
 
-print(*arr, "steps =", count)
+print(*list, "steps =", count)
