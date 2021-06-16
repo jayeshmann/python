58535bcf9ae5f5e0e2e9748eba9298110dff4c49
Jayesh Mann <jayeshmann06@gmail.com>
2021-06-16 09:58:41 +0530

    evenOdd using def

--- evenOdd.py
+++ evenOdd.py
@@ -1,3 +1,13 @@
 a = 46
 
-print("even" if a % 2 == 0 else "odd")
+# print("even" if a % 2 == 0 else "odd")
+
+
+def evenOdd(x):
+    if(x % 2 == 0):
+        print("even")
+    else:
+        print("odd")
+
+
+evenOdd(a)
