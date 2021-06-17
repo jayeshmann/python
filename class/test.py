1a70c20aaec22ccd3ee832d6fac078928d95fff4
Jayesh Mann <jayeshmann06@gmail.com>
2021-06-17 10:49:43 +0530

    list insert()

--- class/test.py
+++ class/test.py
@@ -2,5 +2,16 @@ def adding(a, b, c):
     print(a, "+", b, "+", c, "=", a+b+c)
 
 
-adding(4, c=2, b=3)
-adding(4, 2, b=3)
+def strangeListFun(n):
+    strangeList = []
+
+    for i in range(0, n):
+        strangeList.insert(0, i)
+
+    return strangeList
+
+
+# adding(4, c=2, b=3)
+# adding(4, 2, b=3)
+
+print(strangeListFun(5))
