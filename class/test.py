ad8e2ec92b7b5cb7b0f2f787a88a5a00ed745d58
Jayesh Mann <jayeshmann06@gmail.com>
2021-06-17 11:02:40 +0530

    global var

--- class/test.py
+++ class/test.py
@@ -1,17 +1,10 @@
-def adding(a, b, c):
-    print(a, "+", b, "+", c, "=", a+b+c)
+
+def myFun():
+    global var
+    var = False
+    print("IsVisible?", var)
 
 
-def strangeListFun(n):
-    strangeList = []
-
-    for i in range(0, n):
-        strangeList.insert(0, i)
-
-    return strangeList
-
-
-# adding(4, c=2, b=3)
-# adding(4, 2, b=3)
-
-print(strangeListFun(5))
+var = True
+myFun()
+print(var)
