925980cd260b7f2acde390c25e1ff2cf5eecffbb
Jayesh Mann <jayeshmann06@gmail.com>
2021-06-17 13:53:20 +0530

    more with functions

--- class/test.py
+++ class/test.py
@@ -1,9 +1,11 @@
 
-def myFun(myList1):
+def myFun(myList1, *args, **kwargs):
     print(myList1)
-    myList1 = [0, 1]
+    print(args)
+    print(kwargs)
+    myList1.insert(0, 99)
 
 
 myList2 = [2, 3]
-myFun(myList2)
+myFun(myList2[:], "meow", named="meow meow")
 print(myList2)
