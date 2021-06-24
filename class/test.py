37216a503c9ce3e2b938c67dac98724d4a9925d3
Jayesh Mann <jayeshmann06@gmail.com>
2021-06-24 11:00:18 +0530

    chain output in list form

--- class/test.py
+++ class/test.py
@@ -2,8 +2,8 @@ from itertools import chain
 
 test_list = ["meow", ' ', 'm', 'e', 'o', 'w']
 
-print(*test_list)
+print(test_list)
 
 res = list(chain.from_iterable(test_list))
 
-print(*res)
+print(res)
