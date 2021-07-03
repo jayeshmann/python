ab9705eeb48d25a4ee4165cfb1e6856bd102094a
Jayesh Mann <jayeshmann06@gmail.com>
2021-07-04 01:17:33 +0530

    better

--- test1.py
+++ test1.py
@@ -1,21 +1,15 @@
-""" def checkPrime(p):
+def checkPrime(p):
     for i in range(2, p):
         if p % i == 0:
             return False
     return True
- """
+
 
 n = int(input("Enter n > "))
 l1 = []
-count = 0
 x = 2
-while count != n:
-    b = True
-    for i in range(2, x):
-        if x % i == 0:
-            b = False
-    if(b):
-        count += 1
+while len(l1) != n:
+    if(checkPrime(x)):
         l1.append(x)
     x += 1
 
