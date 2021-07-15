dc65bff51c66e9d8e3b6016f59ed90a3fa7e22c9
Jayesh Mann <jayeshmann06@gmail.com>
2021-07-15 10:05:44 +0530

    private class member

--- class/test.py
+++ class/test.py
@@ -1,11 +1,31 @@
 class Cat:
+
+    __mostFamousBreed = 'Persian'
+
     def __init__(self, breed='Persian'):
         self.breed = breed
 
     def getBreed(self):
         return self.breed
 
+    def getMostFamous(self):
+        return Cat.__mostFamousBreed
 
-mew = Cat('Chartreux')
+    def setMostFamous(self, newFamous):
+        Cat.__mostFamousBreed = newFamous
 
-print(f"{mew.getBreed()} cat")
+
+meow = Cat('Chartreux')
+
+print(f"{meow.getBreed()} cat")
+print(f"{meow.breed}")
+print(f"{meow.__dict__}")
+print(f"Most famous: {meow.getMostFamous()}")
+try:
+    print(f"{meow.__mostFamousBreed}")
+except AttributeError:
+    print("You tried to access private member")
+
+meow.setMostFamous('British shorthair')
+
+print(f"Most famous: {meow.getMostFamous()}")
