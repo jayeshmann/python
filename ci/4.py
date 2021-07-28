d8b24663b0d94f4b285d36027f4851712a4df9d7
Jayesh Mann <jayeshmann06@gmail.com>
2021-07-28 10:38:35 +0530

    better algo

--- ci/4.py
+++ ci/4.py
@@ -1,18 +1,11 @@
-"""
-efficient function to return maximum occurring character in the input string e.g., if input string is “test” then function should return ‘t’
-"""
-
-
-def max_char(string):
-    freq = {}
-    for char in string:
-        freq[char] = freq.get(char, 0) + 1
+def max_char(str):
     max_char = ''
     max_count = 0
-    for i in freq:
-        if freq.get(i) > max_count:
+    for i in str:
+        count = str.count(i)
+        if count > max_count:
+            max_count = count
             max_char = i
-            max_count = freq.get(i)
     return max_char
 
 
