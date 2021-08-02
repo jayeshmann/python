be3c75ac0a21bd778be0a50a51c949e0c2dda485
Jayesh Mann <jayeshmann06@gmail.com>
2021-08-02 14:37:39 +0530

    implementation remains

---
+++ test6.py
@@ -0,0 +1,13 @@
+#
+# Complete the 'countHighlyProfitableMonths' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY stockPrices
+#  2. INTEGER k
+#
+# input:
+# stockPrices = [1, 2, 3, 3, 4, 5]
+# k = 3
+# output:
+# 2
