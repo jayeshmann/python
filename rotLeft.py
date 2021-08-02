bee9888d0210317d7e89c9905c54851c88eaa0ba
Jayesh Mann <jayeshmann06@gmail.com>
2021-08-02 14:35:23 +0530

    Array rotate: 1 liner in python

---
+++ rotLeft.py
@@ -0,0 +1,11 @@
+# Complete the 'rotLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+#
+
+def rotLeft(a, d):
+    # Write your code here
+    return a[d:] + a[:d]
